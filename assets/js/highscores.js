978e12fcecdff65ff7b6af72214008d6e553a2fd
Gwenn RT <gwenncreative@gwenns-MBP.attlocal.net>
2023-12-18 21:51:48 -0600

    removed code that was not functioning

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -11,20 +11,3 @@ clearScores.addEventListener('click', function() {
     localStorage.clear();
     location.reload();
 });
-
-// variables for score conatiner, high scores, and storing them
-var scoreContainer = document.querySelector('#quiz-scores');
-var highscores = document.querySelector('#list-scores');
-
-var storeScores = localStorage.getItem('storeScores');
-storeScores = JSON.parse(storeScores);
-
-if(storeScores !== false) {
-    for (var i = 0; i < storeScores.length; i++) {
-        var printScore = document.createElement('li');
-        printScore.setAttribute('id','scoreLi');
-        printScore.textContent = printScore[i].initials + ' ' + storeScores[i].scores;
-
-        highscores.appendChild(printScore);
-    }
-};
